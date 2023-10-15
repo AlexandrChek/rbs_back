aca15edebbd8bdc3f897b3cbf2dac41da0fa79bb
Chekcheev_Alexandr <chekcheev.alex@gmail.com>
2023-10-15 23:03:06 +0300

    GET delited

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ const server = http.createServer((req, res) => {
     const origin = 'http://localhost:8080'
     //'https://alexandrchek.github.io' - github account
 
-    const myHeaders = {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': origin
-    }
-
     if(req.method === 'OPTIONS') {
         res.writeHead(200, {
             'Access-Control-Allow-Origin': origin,
@@ -20,24 +15,24 @@ const server = http.createServer((req, res) => {
         return
     }
 
-    if(req.method === 'GET') {
-        let result = ''
-        if(req.url === '/stars') {
-            result = JSON.stringify(getStars())
-        }
-        if(req.url === '/search') {
-            result = JSON.stringify(getList())
-        }
-        res.writeHead(200, myHeaders)
-        res.end(result)
-    }
-
     if(req.method === 'POST') {
         let request = ''
         req.on('data', chank => request += chank)
         req.on('end', () => {
-            const result = JSON.stringify(search(request))
-            res.writeHead(200, myHeaders)
+            let result = ''
+
+            if(request === '/stars') {
+                result = JSON.stringify(getStars())
+            } else if(request === '/search') {
+                result = JSON.stringify(getList())
+            } else {
+                result = JSON.stringify(search(request))
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': origin
+            })
             res.end(result)
         })
     }
